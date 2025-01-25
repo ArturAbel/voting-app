1bc0c47787695f4c0c29a1336d98e7f4e83a930f
Artur Abel <abelartur.mail@gmail.com>
2025-01-25 18:26:36 +0200

    MYPOLLS:Addded may polls page and filter bar dark mode

--- src/components/UI/Chips/PollCardChip/PollCardChip.jsx
+++ src/components/UI/Chips/PollCardChip/PollCardChip.jsx
@@ -1,7 +1,12 @@
+import { useThemeStyles } from "../../../../hooks/useThemeStyles";
 import { CHIP } from "../../../../constants/data";
-import styles from "./styles.module.css";
+
+import lightStyles from "./lightStyles.module.css";
+import darkStyles from "./darkStyles.module.css";
 
 const PollCardChip = ({ text, type }) => {
+  const styles = useThemeStyles(lightStyles, darkStyles);
+
   return <div className={`${styles.chip} ${type === CHIP.visibility ? styles.visibility : styles.status}`}>{text}</div>;
 };
 
--- src/components/UI/ProgressBar/CardProgressBar/CardProgressBar.jsx
+++ src/components/UI/ProgressBar/CardProgressBar/CardProgressBar.jsx
@@ -1,6 +1,11 @@
-import styles from "./styles.module.css";
+import { useThemeStyles } from "../../../../hooks/useThemeStyles";
+
+import lightStyles from "./lightStyles.module.css";
+import darkStyles from "./darkStyles.module.css";
 
 const CardProgressBar = ({ votes, maxVotes, percent }) => {
+  const styles = useThemeStyles(lightStyles, darkStyles);
+
   return (
     <progress className={styles.progress} value={votes} max={maxVotes}>
       {percent}
